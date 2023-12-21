3461fb2a18caed3b9acf73c4497251c0534217ed
leiixia <76995945+leiixia@users.noreply.github.com>
2023-12-21 16:32:13 +0100

    Add files via upload

---
+++ projet9/src/main/java/com/example/projet9/Projet9Application.java
@@ -0,0 +1,13 @@
+package com.example.projet9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet9Application.class, args);
+	}
+
+}
